c7d66bda115f3cfee698eff713c9d7abd8a39c1d
ekudram <ekudram@hotmail.com>
2026-05-21 11:29:00 -0600

    Captolamia command added new tips

--- [CAP] Chat Interactive/Commands/TestCommands.cs
+++ [CAP] Chat Interactive/Commands/TestCommands.cs
@@ -16,6 +16,7 @@
 // along with CAP Chat Interactive. If not, see <https://www.gnu.org/licenses/>.
 // A simple test command that responds with a greeting message
 using System;
+using Verse;
 
 namespace CAP_ChatInteractive.Commands.TestCommands
 {
@@ -36,6 +37,7 @@ namespace CAP_ChatInteractive.Commands.TestCommands
         public override string Execute(ChatMessageWrapper messageWrapper, string[] args)
         {
             var globalChatSettings = CAPChatInteractiveMod.Instance.Settings.GlobalSettings;
+
             // Check if the user is you by username AND platform ID
             bool isCaptoLamia = messageWrapper.Username == "captolamia" &&
                                messageWrapper.PlatformUserId == "58513264" &&
@@ -43,7 +45,28 @@ namespace CAP_ChatInteractive.Commands.TestCommands
 
             if (!isCaptoLamia)
             {
-                return $"Sorry {messageWrapper.DisplayName}, this command is not available. 👀";
+                // RICS tips for regular viewers
+                var tips = new[]
+                {
+                    "You can have multiple Rimworld Lockers",
+                    "Rimworld Lockers have settings so you can have Lockers in different areas receiving specific items.",
+                    "!mypawn weapon will give you weapon stats for the weapon your pawn is currently holding.",
+                    "!pricecheck [item name] [quality] [material] [quantity] will give you the market value of that item.",
+                    "!storage [item name] will show you the colony inventory of specific items.",
+                    "!weather [weather] will change in the colony",
+                    "!karmasettings will show you your current karma settings.",
+                    "!purchaselist will tell you the streamers Github link where they have a list of items they want to be purchased for them in the game.",
+                    "!help will show you a wiki link to the list of available commands and how to use them.",
+                    "!modversion will show you the current version of RICS that is running.",
+                    "!study will show you the current anomaly research project being worked on in the colony.",
+                    "!research will show you the current research project being worked on in the colony.",
+                    "!research [project name] will show you the progress of that research project in the colony.",
+
+                    // Add more tips here later - they will be picked randomly
+                };
+
+                string randomTip = tips[Rand.Range(0, tips.Length)];
+                return $"RICS Tip: {randomTip}";
             }
 
             return $"😸 Hello {messageWrapper.DisplayName}! RICS {globalChatSettings.modVersion}. The MOD Developer is present in chat! ";
